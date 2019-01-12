a29ae49698eac53b56db9e0f96af12f631dee450
Gregory Rouleau <grouleau@vols.utk.edu>
2019-01-12 14:11:05 -0500

    fixed increment bug in argument parsing

--- parallel/egoParallel.go
+++ parallel/egoParallel.go
@@ -25,7 +25,7 @@ func main() {
 	// Options
 	precPtr := flag.Int("p", 10001, "Precision for calculations")
 	iterPtr := flag.Uint64("i", 1625, "Value of infinity")
-	increment = *flag.Uint64("increment", 64, "increment size")
+	incrementPtr := flag.Uint64("increment", 64, "increment size")
 	hard := flag.Bool("hard", false, "Stress your hardware more, more iterations! Forces set iterations and precison, overiding any set.")
 	debug := flag.Bool("debug", false, "Used for debugging. This will write to log.txt")
 	flag.Parse()
@@ -33,6 +33,7 @@ func main() {
 	// Iterations
 	precision = *precPtr
 	iterations = *iterPtr
+	increment = *incrementPtr
 	if *hard {
 		iterations = 4288
 		precision = 30001
